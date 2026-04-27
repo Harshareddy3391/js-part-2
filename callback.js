fb092f9424fa9f38c3159f7b36cec3f1a91bb73e
Harshareddy3391 <thotaharshareddy@gmail.com>
2026-04-27 18:29:34 +0530

    fun

---
+++ callback.js
@@ -0,0 +1,10 @@
+//callback function is function that passed an argument to another function and excute later usuvally after task complete..
+
+
+//callback function or high order function...
+
+
+function one(){
+
+    
+}
